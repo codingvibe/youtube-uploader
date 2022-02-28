760719676c40be3838e3921a9a864666ccd20a67
Colby <colby.sites@gmail.com>
2022-02-27 20:18:42 -0600

    Adding setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='youtube_uploader',
+      version='1.0',
+      description='Upload a video to YouTube',
+      author='CodingVibe',
+      url='https://github.com/codingvibe/youtube-uploader'
+     )
